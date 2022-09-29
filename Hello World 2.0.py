9bc9119b963f1a540016a90db7b922469328ff20
Scrimzox <83733410+Scrimzox@users.noreply.github.com>
2022-09-28 20:54:37 -0400

    Delete Hello World 2.0.py

--- Hello World 2.0.py
+++
@@ -1,6 +0,0 @@
-Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("Hello my name is Sahmi")
-Hello my name is Sahmi
->>> print("Hello my name is Sahmi Noorzaie")
-Hello my name is Sahmi Noorzaie
